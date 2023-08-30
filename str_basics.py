b81af2f055df7fcd52ffee429dcd89e8463c6007
Carlos Adrian Denegri Martinez <cadenegr@c4c6c4.42berlin.de>
2023-08-30 14:20:27 +0200

    string_basics

---
+++ str_basics.py
@@ -0,0 +1,47 @@
+#TASK 1
+
+# city = "London"
+
+# print(city)
+
+#TASK 2
+
+# city = "Berlin"
+# population = 3650000
+
+# print(f"{city}:", population)
+
+#TASK 3
+
+# city = "london"
+# capitalize = city.capitalize()
+# is_capitalize = True
+# population = 9000000
+
+# print(f"City: {capitalize} ({is_capitalize})" )
+# print(f"Population: {population}")
+
+#TASK 4
+
+# text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+# substring = "capital"
+
+# index = text.find(substring) #it finds the str[index] of the first letter of the substring
+
+# print(f"{substring}: {index}")
+
+#TASK 5
+
+# text = "Berlin straddles the banks of the Spree, which flows into the Havel (a tributary of the Elbe) in the western borough of Spandau."
+# word_list = text.split()
+
+# print(word_list)
+
+#TASK 6
+
+# text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+
+# new_text = text.replace("capital", "capital of Germany")
+
+# print(new_text)
+
